81526178d611f6a8a7ef30f70f59befefae6b791
dunderburken <dunderburken@gmail.com>
2013-11-01 15:48:34 +0100

    IIS Default document

--- LateToMeeting.Web/App_Start/RouteConfig.cs
+++ LateToMeeting.Web/App_Start/RouteConfig.cs
@@ -12,11 +12,10 @@ namespace LateToMeeting.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{id}"
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
